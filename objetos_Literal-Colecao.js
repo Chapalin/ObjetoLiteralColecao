5c72fb5d45b806f8d5d3d5ab20f24917ac5dc2a2
Paulo Alexandre <84683624+Chapalin@users.noreply.github.com>
2022-10-19 10:55:40 -0300

    Criando um objeto literal e uma coleção de objetos

---
+++ objetos_Literal-Colecao.js
@@ -0,0 +1,22 @@
+// var usuario = {
+//     id: 1,
+//     name: "Paulo",
+//     age: 21
+// }
+
+// var clasificacaoEtoria = usuarios.age 
+
+
+// if (clasificacaoEtoria >= 18) {
+//     console.log("Você já pode ser preso!!")
+// } else {
+//     console.log("Faça muita merda, você ainda é novo:p")
+// }  
+
+var usuarios = [
+    {id: 1, name: "Papis", age: 69, profissao: "o velhinho é aposentado :)" },
+    {id: 2, name: "Paulo", age: 21, profissao: "O melhor desenvolvedor já visto" },
+    {id: 3, name: "Pedro", age: 26, profissao: "Vigilante braboor" }
+]
+
+ console.log(usuarios[0])
